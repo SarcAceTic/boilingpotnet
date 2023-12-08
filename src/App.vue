82f5fe1497901d8dfa8eddb252b81666f477510a
SarcAceTic <sarcacetic@gmail.com>
2023-12-08 11:37:36 +0700

    Added typography and header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import HeaderNavVue from './components/HeaderNav.vue';
 </script>
 
 <template>
   <v-app>
+    <HeaderNavVue />
     <v-main><RouterView /></v-main>
   </v-app>
 </template>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <v-app-bar elevation="1">
+    <v-app-bar-nav-icon>
+      <v-icon size="40">mdi-pot-steam</v-icon>
+    </v-app-bar-nav-icon>
+    <v-app-bar-title><h3>boilingpot.NET</h3></v-app-bar-title>
+    <v-btn class="w-25 border text-none">
+        <template v-slot:prepend>
+            <v-icon>mdi-magnify</v-icon>
+        </template>
+        <p>Search</p>
+    </v-btn>
+  </v-app-bar>
+</template>
