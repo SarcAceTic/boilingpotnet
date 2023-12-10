1939ef9cc34e1b7210c0aa6fed3c1eb53e7737a3
SarcAceTic <sarcacetic@gmail.com>
2023-12-10 17:35:34 +0700

    Added recommended recipe section

--- src/components/RecommendedRecipes.vue
+++ src/components/RecommendedRecipes.vue
@@ -1,6 +1,9 @@
 <script setup>
 import axios from 'axios'
-import myKey from '/sensitive-information.json'
+import { RouterLink } from 'vue-router'
+
+const apiResp = await fetch('/sensitive-information.json')
+const api = await apiResp.json()
 
 const options = {
   method: 'GET',
@@ -10,7 +13,7 @@ const options = {
     query: 'soup'
   },
   headers: {
-    'x-api-key': myKey.myCookingKey,
+    'x-api-key': api.myCookingKey,
     'Content-Type': 'application/json'
   }
 }
@@ -24,9 +27,11 @@ console.log(response)
       <h2 class="mb-4">Recommended Soups</h2>
       <v-row>
         <v-col cols="4" v-for="recipe in response.data.results.slice(3, 6)">
-          <v-card height="100%">
-            <v-img class="w-100 h-auto" :src="recipe.image"></v-img>
-          </v-card>
+          <router-link :to="{ name: 'RecipeDetail', params: { recipeId: parseInt(recipe.id) } }">
+            <v-card height="100%">
+              <v-img class="w-100 h-auto" :src="recipe.image"></v-img>
+            </v-card>
+          </router-link>
           <p class="text-center">{{ recipe.title }}</p>
         </v-col>
       </v-row>
